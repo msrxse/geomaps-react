112fce55307ee5a04c2f4281fb6c2a70b2eb1576
msr <msrxse@gmail.com>
2025-05-12 18:00:06 +0100

    build(client): initial scaffolding

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { type UserConfig, defineConfig } from 'vite'
+import { compression } from 'vite-plugin-compression2'
+import { qrcode } from 'vite-plugin-qrcode'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  envDir: './env/',
+  plugins: [
+    react(),
+    qrcode(),
+    compression({
+      algorithm: 'gzip',
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    target: 'esnext',
+    minify: true,
+    cssTarget: 'esnext',
+    cssMinify: true,
+    cssCodeSplit: true,
+    modulePreload: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react/jsx-runtime', 'react-dom'],
+        },
+      },
+    },
+  },
+}) satisfies UserConfig
