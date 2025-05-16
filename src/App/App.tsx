2d500099217de1f3ed1bda1a7ef43e646a39b165
msr <msrxse@gmail.com>
2025-05-16 17:19:13 +0100

    build(client): add useDimensions

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,21 @@
+import { useRef } from 'react'
+
 import { type FeatureCollection } from 'geojson'
+
+import { useDimensions } from '@/App/map/useDImensions'
 
 import Map from './map/Map'
 import { data } from './map/data'
 
-const App = ({ width = 940, height = 600 }) => {
-  return <Map width={width} height={height} data={data as FeatureCollection} />
+const App = () => {
+  const chartRef = useRef<HTMLDivElement>(null)
+  const dimensions = useDimensions(chartRef)
+
+  return (
+    <div className="h-screen w-full" ref={chartRef}>
+      <Map width={dimensions.width} height={dimensions.height} data={data as FeatureCollection} />
+    </div>
+  )
 }
 
 export { App }
